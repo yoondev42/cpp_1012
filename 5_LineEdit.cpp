8beb47829069b1fae9d7e00e200ffb2b180829e1
Chansik Yun <chansigi@gmail.com>
2020-10-12 14:23:43 +0900

    .

--- 5_LineEdit.cpp
+++ 5_LineEdit.cpp
@@ -27,9 +27,47 @@ int getch(void)
     return ch;
 }
 
+// #include <conio.h>
+// int main() {
+//   while (1) {
+//     int ch = getch();
+//     printf("%c\n", ch);
+//   }
+// }
+
+//---------------------------------
+// GUI 도구인 Edit 박스(LineEdit, EditText)를 만들어봅시다.
+#include <iostream>
+#include <string>
+using namespace std;
+
+class LineEdit {
+  string data;
+public:
+  string getData() {
+    data.clear();
+
+    while (1) {
+      char c = getch();
+      
+      if (c == 13)  // enter
+        break;
+
+      if (isdigit(c)) {
+        data.push_back(c);
+        cout << c;
+      }
+    }
+
+    cout << endl;
+    return data;
+  }
+};
+
 int main() {
+  LineEdit edit;
   while (1) {
-    int ch = getch();
-    printf("%c\n", ch);
+    string s = edit.getData();
+    cout << "Input: " << s << endl;
   }
 }
