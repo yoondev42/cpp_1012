67903443482f401bf64a4f167f88ab453409c85a
Chansik Yun <chansigi@gmail.com>
2020-10-12 11:34:08 +0900

    .

--- 2_접근변경자2.cpp
+++ 2_접근변경자2.cpp
@@ -17,10 +17,18 @@ using namespace std;
 
 // OOP에서 기존 클래스를 재사용하는 방법
 // 1. 상속
-// 2. 
 
+
+// 라이브러리 설계 원칙
+// => 쉽게 사용할 수 있도록 만드는 것도 중요하지만,
+//    잘못 사용하기 어렵게 만들어야 한다.
+
+// private 상속: 부모로부터 구현은 물려받지만(자식이 내부적으로 사용하지만)
+//              인터페이스는 물려 받지 않겠다.(부모 함수를 외부로 노출하지 않겠다.)
+// => 접근 변경자를 통한 문법은 OOP언어가 범용적으로 제공하는 기능이 아닙니다.
+#if 0
 template <typename T>
-class stack : public list<T> {
+class stack : private list<T> {
 public:
   void push(const T& a) { 
     list<T>::push_back(a);
@@ -34,9 +42,43 @@ public:
     return list<T>::back();
   }
 };
+#endif
+
+#include <vector>
+#include <deque>
+#include <stack>
+//      => list와 vector의 절충형태(연속된 메모리로 구현된 리스트)
+
+// S/W의 재사용은 상속과 포함이 있습니다. => 포함이 좋을때가 많다.
+// 2. 포함
+//   => 공개하고자 하는 기능을 명시적으로 제공해야 한다.
+//   => C++ STL의 stack은 list가 아닌 다른 컨테이너를 이용할 수 있는 설계도 적용되어 있습니다.
+
+template <typename T, typename C = deque<T>>
+class Stack {
+  C st;
+public:
+  void push(const T& a) { 
+    st.push_back(a);
+  }
+
+  void pop() {
+    st.pop_back();
+  }
+
+  T& top() {
+    return st.back();
+  }
+};
 
 int main() {
-  stack<int> s;
+  stack<int, list<int>> s;
+  stack<int, vector<int>> s2;
+  
+  // s.pop_back();      // list의 기능을 이용한다!
+  // s.push_front(20);
+
+
   s.push(10);
   s.push(20);
   s.push(30);
