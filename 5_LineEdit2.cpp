859e6b5a4573b4575cbf29fef19a67e3b2a1f18e
Chansik Yun <chansigi@gmail.com>
2020-10-12 14:33:39 +0900

    .

--- 5_LineEdit2.cpp
+++ 5_LineEdit2.cpp
@@ -1,8 +1,4 @@
 // 5_LineEdit.cpp
-
-// #include <conio.h>
-//    getch
-
 #include <termios.h>
 #include <stdio.h>
 
@@ -27,23 +23,21 @@ int getch(void)
     return ch;
 }
 
-// #include <conio.h>
-// int main() {
-//   while (1) {
-//     int ch = getch();
-//     printf("%c\n", ch);
-//   }
-// }
-
-//---------------------------------
 // GUI 도구인 Edit 박스(LineEdit, EditText)를 만들어봅시다.
 #include <iostream>
 #include <string>
 using namespace std;
 
+// 1. Validation의 정책은 변경이 가능해야 합니다.
+//   방법 1.
+//    => 변하는 것을 가상함수화 한다. - template method pattern
+//     : 변하지 않는 코드(공통성)에서 변해야 하는 것(가변성)이 있다면
+//       변하는 것을 가상함수로 뽑아낸다.
 class LineEdit {
   string data;
 public:
+  virtual bool validate(char c) { return isdigit(c); }
+
   string getData() {
     data.clear();
 
@@ -53,7 +47,7 @@ public:
       if (c == 10)
         break;
 
-      if (isdigit(c)) {
+      if (validate(c)) {
         data.push_back(c);
         cout << c;
       }
@@ -64,8 +58,17 @@ public:
   }
 };
 
+// 이제 정책을 변경하고 싶다면 파생 클래스에서 가상 함수를 재정의하면 됩니다.
+class AddressLineEdit : public LineEdit {
+public:
+  bool validate(char c) override {
+    return true;
+  }
+};
+
 int main() {
-  LineEdit edit;
+  // LineEdit edit;
+  AddressLineEdit edit;
   while (1) {
     string s = edit.getData();
     cout << "Input: " << s << endl;
