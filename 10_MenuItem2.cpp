9f22e969f64220493d41cc50551e8c71bdd41dee
Chansik Yun <chansigi@gmail.com>
2020-10-13 11:07:52 +0900

    .

--- 10_MenuItem2.cpp
+++ 10_MenuItem2.cpp
@@ -1,20 +1,46 @@
 #include <iostream>
 using namespace std;
 
-// 이벤트 처리 설계 방법
-//  : 매우 중요합니다.
+// 1. 인터페이스 기반의 리스너 개념
+//    : Java, C++(Tizen)
+
+// 메뉴 이벤트를 처리하고 싶은 모든 클래스는 아래 인터페이스를 구현해야 한다.
+struct IMenuListener {
+  virtual ~IMenuListener() {}
+
+  virtual void onCommand() = 0;
+};
 
 class MenuItem {
+  IMenuListener* listener;
 public:
-  MenuItem() {}
+  MenuItem() : listener(nullptr) {}
+
+  void setListener(IMenuListener* p) { listener = p; }
 
   virtual void command() {
-    // 메뉴가 선택된 사실을 다시 외부로 알려야 한다.
-    // "객체가 외부로 이벤트를 발생한다." 라고 표현합니다.
+    if (listener) {
+      listener->onCommand();
+    }
+  }
+};
+
+class Dialog : public IMenuListener {
+  void open() {
+    cout << "Dialog open" << endl;
+  }
+
+  // 메뉴 이벤트가 발생하게 되면, 아래 함수가 호출될 것이고, 아래 함수에서 원하는 동작을 수행하면 됩니다.
+  void onCommand() override {
+    open();
   }
 };
 
 int main() {
   MenuItem m;
+  Dialog dlg;
+
+  m.setListener(&dlg);
+
   m.command();
 }
