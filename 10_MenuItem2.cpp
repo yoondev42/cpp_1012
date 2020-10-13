64cbdf5d00aae76a3f393ee703e8022f1e7e13e1
Chansik Yun <chansigi@gmail.com>
2020-10-13 11:14:40 +0900

    .

--- 10_MenuItem2.cpp
+++ 10_MenuItem2.cpp
@@ -8,19 +8,20 @@ using namespace std;
 struct IMenuListener {
   virtual ~IMenuListener() {}
 
-  virtual void onCommand() = 0;
+  virtual void onCommand(int id) = 0;
 };
 
 class MenuItem {
+  int id;
   IMenuListener* listener;
 public:
-  MenuItem() : listener(nullptr) {}
+  MenuItem(int i) : id(i), listener(nullptr) {}
 
   void setListener(IMenuListener* p) { listener = p; }
 
   virtual void command() {
     if (listener) {
-      listener->onCommand();
+      listener->onCommand(id);
     }
   }
 };
@@ -30,17 +31,35 @@ class Dialog : public IMenuListener {
     cout << "Dialog open" << endl;
   }
 
+  void close() {
+    cout << "Dialog close" << endl;
+  }
+
   // 메뉴 이벤트가 발생하게 되면, 아래 함수가 호출될 것이고, 아래 함수에서 원하는 동작을 수행하면 됩니다.
-  void onCommand() override {
-    open();
+  //  문제점: 어떤 메뉴 객체로부터 이벤트가 발생했는지 여부를 알 수 없다.
+  //   => 인터페이스에 어떤 객체로부터 이벤트가 발생했는지 식별하는 정보가 추가되어야 한다!
+  void onCommand(int id) override {
+    switch (id) {
+      case 100:
+        open();
+        break;
+      case 200:
+        close();
+        break;
+    }
+    
   }
 };
 
 int main() {
-  MenuItem m;
+  MenuItem m(100);
+  MenuItem m2(200);
+
   Dialog dlg;
 
   m.setListener(&dlg);
+  m2.setListener(&dlg);
 
   m.command();
+  m2.command();
 }
