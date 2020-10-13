35b29ead9f3d3ea88def2ec25ddc372dadfc4d68
Chansik Yun <chansigi@gmail.com>
2020-10-13 10:36:25 +0900

    .

--- 9_Composite.cpp
+++ 9_Composite.cpp
@@ -2,6 +2,47 @@
 // 복합 객체 - Folder
 //     단일 인터페이스 - getSize()
 //     공통의 부모    - Item
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Item {
+  string name;
+public:
+  Item(const string& s) : name(s) {}
+  virtual ~Item() {}
+
+  virtual int getSize() = 0;
+};
+
+class File : public Item {
+  int size;
+public:
+  File(const string& s, int sz) : Item(s), size(sz) {}
+
+  int getSize() override {
+    return size;
+  }
+};
+
+class Folder : public Item {
+  vector<Item*> v;
+public:
+  Folder(const string& s) : Item(s) {}
+
+  void addItem(Item* p) { v.push_back(p); }
+
+  int getSize() {
+    int sum = 0;
+    for (Item* e : v) {
+      sum += e->getSize();
+    }
+    return sum;
+  }
+};
+
+
 
 int main()
 {
