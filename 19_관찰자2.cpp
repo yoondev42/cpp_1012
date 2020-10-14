14964703652d3c70d972f2538634748fedd00987
Chansik Yun <chansigi@gmail.com>
2020-10-14 14:34:31 +0900

    .

--- 19_관찰자2.cpp
+++ 19_관찰자2.cpp
@@ -4,10 +4,12 @@ using namespace std;
 
 
 // 관찰자의 인터페이스
+class Subject;
 struct IObserver {
   virtual ~IObserver() {}
 
-  virtual void onUpdate(int* data, int len) = 0;
+  Subject* pSubject;
+  virtual void onUpdate() = 0;
 };
 
 
@@ -15,25 +17,32 @@ struct IObserver {
 //  - 관찰자 패턴에서 관찰의 대상(Subject)의 로직은 항상 동일하다.
 //    부모 클래스를 통해 반복되는 로직을 제공해주자.
 
-
-
-class Table {
-  int data[5];
+class Subject {
   vector<IObserver*> v;
 public:
-  Table() { memset(data, 0, sizeof data); }
+  virtual ~Subject() {}
 
   void attach(IObserver* p) {
     v.push_back(p);
+    p->pSubject = this;
   }
 
   void detach(IObserver* p) {}
-
+  
   void notify() {
     for (IObserver* p : v) {
-      p->onUpdate(data, 5);
+      p->onUpdate();
     }
   }
+};
+
+class Table : public Subject {
+  int data[5];
+public:
+  Table() { memset(data, 0, sizeof data); }
+
+  const int* getData() const { return data; }
+  int getSize() const { return 5; }
 
   void edit() {
     while (1) {
@@ -45,10 +54,20 @@ public:
     }
   }
 };
+
 //---------
 class PieGraph : public IObserver {
 public:
-  void onUpdate(int* data, int len) override {
+  void onUpdate() override {
+    // Table* table = dynamic_cast<Table*>(pSubject);
+    // if (table != 0) {}
+    Table* table = static_cast<Table*>(pSubject);
+    // 다운 캐스팅을 수행할 때, 해당 타입에 대해서 확신할 수 있다면,
+    // static_cast를 사용하는 것이 좋다.
+    // => 런타임 오버헤드가 존재하지 않는다.
+    const int* data = table->getData();
+    int len = table->getSize();
+
     cout << "************ Pie ************" << endl;
     for (int i = 0 ; i < len; ++i)
       cout << i << ": " << data[i] << endl;
@@ -57,7 +76,11 @@ public:
 
 class BarGraph : public IObserver {
 public:
-  void onUpdate(int* data, int len) override {
+  void onUpdate() override {
+    Table* table = static_cast<Table*>(pSubject);
+    const int* data = table->getData();
+    int len = table->getSize();
+
     cout << "************ Bar ************" << endl;
     for (int i = 0 ; i < len; ++i)
       cout << i << ": " << data[i] << endl;
