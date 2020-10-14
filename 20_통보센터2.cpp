0b2befc7104362bbfb545d17531081987bdfa2d9
Chansik Yun <chansigi@gmail.com>
2020-10-14 16:34:36 +0900

    .

---
+++ 20_통보센터2.cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>
+using namespace std;
+
+#include <functional>
+// function/ bind의 사용법
+
+// void (*p)();
+void foo() { printf("foo\n"); }
+
+void goo(int a) { printf("goo - %d\n", a); }
+void hoo(int a, int b) { printf("hoo - %d %d\n", a, b); }
+void koo(int a, int b, int c, int d) { printf("koo - %d %d %d %d\n", a, b, c, d); }
+
+int main() {
+  // void (*fp)() = &foo;
+  function<void()> fp = &foo;
+  fp();
+
+  // goo(20);
+  fp = bind(&goo, 20);
+  fp();
+
+  fp = bind(&hoo, 10, 20);
+  fp();
+
+  fp = bind(&koo, 10, 20, 30, 40);
+  fp();
+
+}
