c1eef7ea5a35c5cfe7a169eed4ff0e57a2ccdef3
Chansik Yun <chansigi@gmail.com>
2020-10-14 16:47:53 +0900

    .

--- 20_통보센터2.cpp
+++ 20_통보센터2.cpp
@@ -7,26 +7,63 @@ using namespace std;
 
 // void (*p)();
 void foo() { printf("foo\n"); }
-
 void goo(int a) { printf("goo - %d\n", a); }
+
 void hoo(int a, int b) { printf("hoo - %d %d\n", a, b); }
 void koo(int a, int b, int c, int d) { printf("koo - %d %d %d %d\n", a, b, c, d); }
 
 using namespace std::placeholders;
 
+
+class Dialog {
+public:
+  void f1() { cout << "f1" << endl; }
+  void f2(int a) { cout << "f2" << endl; }
+};
+
+// Dialog
+//   f1 - void(Dialog*)
+//   f2 - void(Dialog*, int)
+
+int main() {
+  Dialog dlg;
+  function<void(Dialog*)> fp;
+  fp = &Dialog::f1;
+  fp(&dlg);
+
+  function<void(Dialog*,int)> fp2;
+  fp2 = &Dialog::f2;
+  fp2(&dlg, 42);
+}
+
+
+
+#if 0
+int main() {
+  function<void(int, int)> fp;
+  fp = &hoo;
+  fp(10, 20);
+
+  fp = bind(&koo, _1, _2, _2, _1);
+  fp(10, 30);
+  // 10 30 30 10
+}
+#endif
+
+#if 0
 int main() {
   function<void(int)> fp;
   fp = &goo;
   fp = bind(&hoo, 30, _1);
   
   // koo를 호출할 때
+  fp = bind(&koo, _1, _1, 10, _1);
   
-  fp(30);
+  fp(20);
   // fp(20) -> koo - 20, 20, 10, 20
   // fp(30) -> koo - 30, 30, 10, 30
-
 }
-
+#endif
 
 #if 0
 int main() {
