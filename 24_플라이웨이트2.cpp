bd05379ac8dc85e7b2100276b347cd1a5dbb479d
Chansik Yun <chansigi@gmail.com>
2020-10-15 13:31:34 +0900

    .

--- 24_플라이웨이트2.cpp
+++ 24_플라이웨이트2.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <string>
 #include <thread>  
+#include <map>
 using namespace std;
 
 // 의도: 속성이 동일한 객체가 다수 생성된다면, 하나의 객체의 '공유'해서 사용하자.
+//     => '캐시'를 도입하자.
+
 // 방법 1.
 //   Image를 직접 생성하는 것이 아니라, Image 객체를 생성하는 역활을 담당하는 객체를 도입하자.
 
@@ -31,11 +34,12 @@ public:
 // 2. 공장은 싱글톤으로 만드는 경우가 많습니다.
 class ImageFactory {
 private:
+  map<string, Image*> cache;
+
   ImageFactory() {}
 
   ImageFactory(const ImageFactory&) = delete;
   ImageFactory& operator=(const ImageFactory&) = delete;
-
 public:
   static ImageFactory& getInstance() {
     static ImageFactory instance;
@@ -45,7 +49,11 @@ public:
   // Image를 생성하는 아래의 함수를 변경한다면,
   // 전체 코드를 수정하지 않고 객체 생성의 정책을 변경하는 것이 가능하다.
   Image* createImage(const std::string& url) {
-    return new Image(url);
+    if (cache[url] == nullptr) {
+      cache[url] = new Image(url);
+    }
+
+    return cache[url];
   }
 };
 
@@ -54,6 +62,6 @@ int main() {
   Image* img1 = ImageFactory::getInstance().createImage("https://a.com/a.png");
   img1->draw();
 
-  Image* img2 = ImageFactory::getInstance().createImage("https://a.com/a.png");
+  Image* img2 = ImageFactory::getInstance().createImage("https://a.com/b.png");
   img2->draw();
 }
