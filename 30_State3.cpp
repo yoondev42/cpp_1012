9e490195a7403b7d89cb3dbd0de3e2e4ba8c339e
Chansik Yun <chansigi@gmail.com>
2020-10-16 10:11:08 +0900

    .

--- 30_State3.cpp
+++ 30_State3.cpp
@@ -2,16 +2,51 @@
 #include <iostream>
 using namespace std;
 
-// 상태 패턴
+// 방법 2. 변하는 것을 가상함수로 - item에 따라 동작은 변한다.
+
 class Hero {
   int gold;
   int itemState;
 public:
-  void Run()    { cout << "Hero Run" << endl;   }
-  void Attack() { cout << "Hero Attack" << endl;}
+  virtual void Run()    { cout << "Hero Run" << endl;   }
+  virtual void Attack() { cout << "Hero Attack" << endl;}
+};
+
+// 아이템의 획득에 따라 동작을 변경하기 위해
+// 상속을 이용해보자.
+class FastHero : public Hero {
+public: 
+  virtual void Run() {
+    cout << "Hero Fast Run" << endl;
+  }
+};
+
+class SlowHero : public Hero {
+public:
+  virtual void Run() {
+    cout << "Hero Slow Run" << endl;
+  }
+};
+
+class SuperFastHero : public Hero {
+public:
+  virtual void Run() {
+    cout << "Hero Super Fast Run" << endl;
+  }
 };
 
 int main() {
   Hero h;
-  h.Attack();
+  FastHero fh;
+  SlowHero sh;
+  SuperFastHero sfh;
+
+  Hero* p = &h;
+  p->Run();
+
+  p = &fh;
+  p->Run();
+
+  p = &sfh;
+  p->Run();  
 }
