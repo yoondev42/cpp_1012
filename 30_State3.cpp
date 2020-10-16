e987a9d76bcf6b1e5f2e64688b3010da63114a27
Chansik Yun <chansigi@gmail.com>
2020-10-16 10:22:36 +0900

    .

--- 30_State3.cpp
+++ 30_State3.cpp
@@ -5,6 +5,7 @@ using namespace std;
 // 방법 2. 변하는 것을 가상함수로 - item에 따라 동작은 변한다.
 
 class Hero {
+public:
   int gold;
   int itemState;
 public:
@@ -35,12 +36,19 @@ public:
   }
 };
 
+// Hero의 상태에 따른 동작이 변경이 되는 것이 아니라,
+// Hero가 변경된 것이다.
+
 int main() {
   Hero h;
+  h.gold = 100;
+
   FastHero fh;
   SlowHero sh;
   SuperFastHero sfh;
 
+  // 객체가 변경되기 때문에 동작이 변경되는 것이다.
+  // 다른 객체 상태가 공유되지 않는다.
   Hero* p = &h;
   p->Run();
 
---
+++ 30_State4.cpp
@@ -0,0 +1,62 @@
+// State Pattern
+#include <iostream>
+using namespace std;
+
+// 방법 3. 변하는 것을 인터페이스 기반 클래스로 뽑아낸다.
+//     => State Pattern
+//       : 구현하는 방법은 Starategy Pattern과 동일하다.
+//         의도: 객체의 상태에 따라 동작을 변경하는 의도
+
+// Item 상태에 따른 영웅의 동작을 정의한 인터페이스
+struct IState {
+  virtual ~IState() {}
+
+  virtual void Run() = 0;
+  virtual void Attack() = 0;
+  // ...
+};
+
+class Hero {
+  int gold;
+  IState* state;
+public:
+  void SetState(IState* p) { state = p; }
+
+  void Run()    { state->Run();     }
+  void Attack() { state->Attack();  }
+};
+
+// 다양한 Item에 따른 동작을 정의한다.
+class NormalState : public IState {
+public:
+  void Run() override {
+    cout << "Hero Run" << endl;
+  }
+
+  void Attack() override {
+    cout << "Hero Attack" << endl;
+  }
+};
+
+class FastState : public IState {
+public:
+  void Run() override {
+    cout << "Hero Fast Run" << endl;
+  }
+
+  void Attack() override {
+    cout << "Hero Fast Attack" << endl;
+  }
+};
+
+int main() {
+  NormalState ns;
+  FastState fs;
+
+  Hero h;
+  h.SetState(&ns);
+  h.Run();
+
+  h.SetState(&fs);
+  h.Run();
+}
